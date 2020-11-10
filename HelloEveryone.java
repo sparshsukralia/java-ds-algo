9b567cacf1683c25ad32765d87c9cc5997eca1b1
Sparsh Sukralia <sparsh.sukralia@gmail.com>
2020-11-11 03:55:22 +0545

    initial commit

---
+++ HelloEveryone.java
@@ -0,0 +1,11 @@
+public class HelloEveryone {
+
+  public static void main(String[] args) {
+
+    int i = 1;
+    for (i = 2; i < 12; i++) {
+      System.out.println(i);
+      System.out.println(i + 1);
+    }
+  }
+}
